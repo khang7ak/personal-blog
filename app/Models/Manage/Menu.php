8cc44f2ecd751ad5440720140f2bef42bd364376
khang7ak <trongkhang280995@gmail.com>
2021-12-02 10:04:34 +0700

    add model Menu

---
+++ app/Models/Manage/Menu.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Manage;
+
+use App\Models\Base\BaseModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends BaseModel
+{
+    use HasFactory;
+}
