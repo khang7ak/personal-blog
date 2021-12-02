db966408cba9f6ebd14f70109f0a210b44f0c2ff
khang7ak <trongkhang280995@gmail.com>
2021-12-02 10:07:23 +0700

    add controller MenuController

--- app/Http/Controllers/Base/BaseController.php
+++ app/Http/Controllers/Base/BaseController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Base;
 
 use App\Http\Controllers\Controller;
+use App\Models\Base\BaseModel;
 use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-    //
+    public function getById($id)
+    {
+        $query = BaseModel::getDetail($id);
+        return $query;
+    }
 }
